0111a011e4efdea758784a1c5f32830cb0ba2f96
sangereaa <75768891+sangereaa@users.noreply.github.com>
2021-05-10 23:54:54 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print("Yap!printing")
+print("I'd much rather you 'not'.")
+print('I"said"do not touch this.')
+print("more")
